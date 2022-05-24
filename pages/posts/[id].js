66ceb95648cae546fabf4e763c16f278ab0268d7
Kennybabs-py <babalolakehinde74@gmail.com>
2022-05-24 19:56:27 +0100

    fixed

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,4 @@
-import Layout from "../../components/layout";
+import Layout from "../../components/Layout";
 import Date from "../../components/date";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import utilStyles from "../../styles/utils.module.css";
